e4aabe1302e04abea5861d2910f7adceff7720fc
Robinkolbenschlag <167406206+Robinkolbenschlag@users.noreply.github.com>
2024-08-02 09:19:21 +0000

    Üben 02.08.24

--- 02_Modify/task1.py
+++ 02_Modify/task1.py
@@ -1,15 +1,21 @@
 import re
+def test():
 
-reg = re.compile("(https?://)?www\.[a-z.-]+\.(de|com)([a-z/]*)")
+    reg = re.compile("(https?://)?www\.[a-z.-]+\.(de|com)([a-z/]*)")
 
-m = reg.match("www.google.com")
-print(m)
+    m = reg.match("www.google.com")
+    print(m)
 
-m = reg.match("https://www.youtube.com")
-print(m)
+    m = reg.match("https://www.youtube.com")
+    print(m)
 
-m = reg.match("http://www.uni-wuerzburg.de")
-print(m)
+    m = reg.match("http://www.uni-wuerzburg.de")
+    print(m)
 
-m = reg.match("www.wiwi.uni-wuerzburg.de/wiba/startseite/")
-print(m)
+    m = reg.match("www.wiwi.uni-wuerzburg.de/wiba/startseite/")
+    print(m)
+form = re.compile("(123)")
+match = form.findall("123123")
+print(match)
+
+
